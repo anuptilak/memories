a542e0ff90c6c6ad406c29b1a30cde653304a3da
Anup Tilak <anuptilak@BCT-LTP-G1113.local>
2023-05-27 19:02:28 +0530

    responsive css, action constants and other small changes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -32,17 +32,17 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant='h5' gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography variant='h5' gutterBottom>{post.message}</Typography>
+                <Typography variant='body2' color="textSecondary" component="p" >{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size='small' color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThubUpAltIcon fontSize='small' />
-                    Like
+                    &nbsp; Like &nbsp;
                     {post.likeCount}
                 </Button>
                 <Button size='small' color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize='small' />
-                    Delete
+                    &nbsp; Delete
                    </Button>
             </CardActions>
         </Card>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,16 @@
+
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch(action.type){
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.playload);
-        case 'LIKE'  :
-        case 'UPDATE':
+        case UPDATE:
             console.log(action.payload._id);
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
